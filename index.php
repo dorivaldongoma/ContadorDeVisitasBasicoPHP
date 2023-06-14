e45b6ba218760cd63353ba99efe77d9247e0a158
dorivaldongoma <dorivaldongoma@gmail.com>
2023-06-14 17:35:58 +0100

    Página básica para visualizar contagem finalizada

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require_once "contador.php"; ?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Contador De Visitas Simples | Dorivaldo Ngoma</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Dorivaldo Ngoma">
+    <meta name="description" content="Contador de Visitas Básico com PHP">
+</head>
+<body>
+    <h1>Número de Acessos: <?php echo $visitas; ?></h1>
+</body>
+</html>
